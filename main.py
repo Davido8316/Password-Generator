80521a2ee9b1cccd3541892a3f4882c3e21fee0a
David <82455204+Davido8316@users.noreply.github.com>
2021-04-15 12:41:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,55 @@
 import secrets
 
 pw = ""
-letter = "!#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
-lengh = int(input("Please enter password lengh: "))
+syntax = ""
+zeichen = ""
 
-for _ in range(lengh):
-    pw = pw+secrets.choice(letter)
-print(pw)
+zeichenBuchstaben = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+zeichenZahlen = "0123456789"
+zeichenSymbole = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+
+
+def program():
+    global pw
+    syntax = int(input("Bitte Inhalt des Passwort festlegen.\n"
+                       "Für Buchstaben = '1'\n"
+                       "Für Zahlen     = '2'\n"
+                       "Für Symbole    = '3'\n"
+                       "Hier Eintragen:"))
+
+    if syntax   == 1:
+        zeichen = zeichenBuchstaben
+
+    elif syntax == 2:
+        zeichen = zeichenZahlen
+
+    elif syntax == 3:
+        zeichen = zeichenSymbole
+
+    elif syntax == 12:
+        zeichen = zeichenBuchstaben + zeichenZahlen
+
+    elif syntax == 21:
+        zeichen = zeichenBuchstaben + zeichenZahlen
+
+    elif syntax == 13:
+        zeichen = zeichenBuchstaben
+
+    elif syntax == 31:
+        zeichen = zeichenBuchstaben + zeichenSymbole
+
+    elif syntax == 23:
+        zeichen = zeichenZahlen + zeichenSymbole
+
+    elif syntax == 32:
+        zeichen = zeichenZahlen + zeichenSymbole
+
+    laenge = int(input("Bitte Passwortlänge eingeben: "))
+    for _ in range(laenge):
+        pw = pw + secrets.choice(zeichen)
+
+program()
+
+print("Dein generiertes Passwort lautet: " +pw)
+print("Beliebige Taste drücken, um das Fenster zu schließen.")
+input()
